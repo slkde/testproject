b9ceae33e37bfbde806c46f5ee5ee2eca785a9bf
不咯人的胖子 <657508740@qq.com>
2017-12-28 16:01:37 +0800

    修改

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,21 +10,30 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', function () {
-    return view('home.home');
-});
+//前台首页
+Route::get('/', 'Home\HomeController@index');
+//前台问题列表
 Route::resource('question', 'Home\QuestionListController');
+//前台回答列表
 Route::resource('answer', 'Home\AnswerController');
+//前台用户注册页面
 Route::get('/user/register', 'Home\UsersController@register');
+//前台用户注册提交
 Route::post('/user/register', 'Home\UsersController@store');
+//前台用户登录
 Route::get('/user/login', 'Home\UsersController@login');
 Route::post('/user/login', 'Home\UsersController@signin');
+//前台用户退出
 Route::get('/logout', 'Home\UsersController@logout');
+//前台邮箱激活
 Route::get('/verify/{confirm_code}', 'Home\UsersController@checkEmail');
+//前台用户头像修改
 Route::resource('/user/photo', 'User\UsersAvatarController');
+//前台用户个人设置
 Route::resource('/user/set', 'User\UsersSetController');
+//前台站内信
 Route::resource('/user/message', 'User\UsersMessageController');
+//前台点赞功能
 Route::get('/question/like/{id}', 'Home\LikeController@like');
 
 
