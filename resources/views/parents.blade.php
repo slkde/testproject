12068c8fb3b36d2743579915d4900659d23f0f09
sunlikun <slkde@sina.com>
2017-12-29 08:49:11 +0800

    new

--- resources/views/parents.blade.php
+++ resources/views/parents.blade.php
@@ -105,7 +105,7 @@
                 @endif
                 <span class="caret"></span></a>
             <ul class="dropdown-menu">
-                <li><a href="{{ url('user/center') }}">个人首页</a></li>
+                <li><a href="{{ url('user/person') }}">个人首页</a></li>
                 <li><a href="{{ url('user/photo') }}">头像设置</a></li>
                 <li><a href="{{ url('user/set') }}">个人设置</a></li>
                 <li><a href="{{ url('user/message') }}">站内信</a></li>
